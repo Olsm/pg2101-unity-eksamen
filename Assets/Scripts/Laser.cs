091d158c4e65c211aaf23ee2a7687bd461792695
OlavOlsm <OlavOlsm@gmail.com>
2015-06-08 18:29:26 +0200

    Script for å bevege/skyte laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	private bool run = false;
+	private float speed = 2f;
+	private Vector2 targetPosition; // = new Vector2(20f, 0f);
+	
+	// Update is called once per frame
+	void Update () {
+		if (run) {
+			// Oppdater posisjonen med lerp for mykere bevegelse
+			transform.position = Vector2.Lerp (transform.position, targetPosition, Time.deltaTime * speed);
+
+			// slett spillobjektet når posisjon x er større enn 10
+			if (transform.position.x > targetPosition.x - 0.1)
+				Destroy (gameObject);
+		}
+	}
+
+	// Skyt laser til posisjonen
+	void Shoot(Vector2 target) {
+		targetPosition = target;
+		run = true;
+	}
+}
