c1b29c73a4398d090a2f0fb9f2281edf83db8c30
OlavOlsm <OlavOlsm@gmail.com>
2015-06-10 05:12:39 +0200

    show timer text

--- Assets/Scripts/BasketYellow.cs
+++ Assets/Scripts/BasketYellow.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BasketYellow : MonoBehaviour {
 
@@ -7,25 +8,30 @@ public class BasketYellow : MonoBehaviour {
 	private float timeLeft = timer;
 	private string timeString;
 	private bool move = false;
-	private float timePoints = 10;
-	private PointsManager gameInfo;
+	private float timePoints = 1;
+	private GameManager gameInfo;
+	public GameObject timerText;
+	public GUIText timeLeftText;
+	private Camera camera;
 	
 	void Awake () {
-		gameInfo = GameObject.Find ("Text_GameInfo").GetComponent<PointsManager> ();
+		gameInfo = GameObject.Find ("Text_GameInfo").GetComponent<GameManager> ();
+		camera = GameObject.Find ("Main Camera").GetComponent<Camera> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		// Update time left
 		if (timeLeft > 0) {
 			timeLeft -= Time.deltaTime;	// remove a secnd from time left
 			timeLeft = Mathf.Clamp (timeLeft, 0, timer);	// prevent from going lower than 0
-			timeString = PointsManager.timeToString (timeLeft);	// tun time into string
-			//timeLeftText.text = "Time left: " + timeString;
+			timeString = GameManager.timeToString (timeLeft);	// tun time into string
+			timeLeftText.text = timeString;
+			timerText.transform.position = camera.WorldToViewportPoint(transform.position + new Vector3(0, 0.7f, 0));
 		}	// game is over if no time left 
 		else if (!move && timeLeft == 0) {
 			move = true;
+			timeLeftText.enabled = false;
 		}
 
 		if (move) {
@@ -41,10 +47,5 @@ public class BasketYellow : MonoBehaviour {
 			gameInfo.AddTime(timePoints);
 			Destroy (gameObject);
 		} 
-	}
-
-	void OnTriggerEnter2D (Collider2D other) {
-		move = true;
-		other.gameObject.transform.parent = transform;
 	}
 }
