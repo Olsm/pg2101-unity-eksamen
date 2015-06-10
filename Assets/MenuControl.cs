2ac2185b550ee62e5c2f7d4b547a7ff04b7e8941
OlavOlsm <OlavOlsm@gmail.com>
2015-06-10 03:05:17 +0200

    Game menu added

---
+++ Assets/MenuControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuControl : MonoBehaviour {
+
+	public Text highScore;
+
+	// Set high score in menu
+	void Start () {
+		highScore.text = "High Score: " + PlayerPrefs.GetFloat ("High Score", 0);
+	}
+
+	// Load first game level
+	public void LoadGame () {
+		Application.LoadLevel("Level1");
+	}
+}
